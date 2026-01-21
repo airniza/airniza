592f4d7cd65bdf8f0256da4da17daec0c862048f
Rahul Kumar <aadiyogietw@gmail.com>
2026-01-21 22:04:28 +0530

    jklj

--- app/(Blog-group)/air-quality/[slug]/route.ts
+++ app/(Blog-group)/air-quality/[slug]/route.ts
@@ -271,7 +271,7 @@ export async function GET(
 
       <footer>
         <div class="container">
-          <p>© 2026 Airniza • Real-time Air Quality Updates & Expert Insights</p>
+          <p>© 2026 Airniza</p>
         </div>
       </footer>
     </body>
